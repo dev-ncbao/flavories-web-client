0b7b61800554fa3a39e00ca23f395d60c5b62d4e
Bao Nguyen <ncbao.dev@gmail.com>
2025-10-07 16:33:55 +0700

    Config

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx';
 import '@fontsource/inter';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
