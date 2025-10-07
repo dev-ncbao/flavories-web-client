af8eb722bc581276316ca344904aa1cd68c7de55
Bao Nguyen <ncbao.dev@gmail.com>
2025-10-07 11:06:58 +0700

    Added react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { Button } from '@mui/joy';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -42,7 +43,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      
+      <Button>Hello world</Button>
     </>
   );
 }
